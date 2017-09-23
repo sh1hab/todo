b6bded583e016de07da1517521834d8bcff21108
sh1hab <shihab9921@gmail.com>
2017-09-23 21:14:10 +0600

    finished functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,31 @@
 $('document').ready(
-	$('li').on("click",function()
+
+	
+
+	$('ul').on("click","li",function()
 	{
-		// console.log( $(this).css("color") );
-		if(  $(this).css("color")=== "rgb(0, 0, 0)" )
+		//on off the class on li
+		$(this).toggleClass("completed");
+	}),
+
+	$('ul').on("click","span",function( event ){
+		//fadesout the li
+		$(this).parent().fadeOut(500,function(){
+			//removes the li
+			$(this).remove();
+		})
+		//stops event bubbling
+		event.stopPropagation();
+	}),
+
+	$("input[type='text']").on("keypress",function( obj ){
+		if( obj.which===13)
 		{
-			$(this).css({
-				textDecoration:"line-through",
-				color:'gray'
-			});
+			// grabbing input val
+			var todotext=$(this).val();
+			// adding a new li
+			$('ul').append("<li> <span> X </span>"+ todotext +" </li>");
 		}
-		else{
-			$(this).css({
-				textDecoration:"none",
-				color:'black'
-			});
-		}
-	}
-	)
-);
+	}),
+
+	);

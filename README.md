764a80af3f7bf4d93d90db3757dea06a1c43e755
sh1hab <shihab9921@gmail.com>
2017-09-25 00:32:28 +0600

    Added Electron

---
+++ README.md
@@ -0,0 +1,24 @@
+
+
+This is a Todo-List application
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/electron/electron-quick-start
+# Go into the repository
+cd electron-quick-start
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+
+
+
+## License
+
+MIT
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,13 @@
-$('document').ready(
+$(document).ready(function(){
 
-	
-
-	$('ul').on("click","li",function()
+	$('ul').on('click','li',function(event)
 	{
 		//on off the class on li
 		$(this).toggleClass("completed");
-	}),
+		
+	});
 
-	$('ul').on("click","span",function( event ){
+	$('ul').on('click','span',function( event ){
 		//fadesout the li
 		$(this).parent().fadeOut(500,function(){
 			//removes the li
@@ -16,16 +15,35 @@ $('document').ready(
 		})
 		//stops event bubbling
 		event.stopPropagation();
-	}),
+	});
 
-	$("input[type='text']").on("keypress",function( obj ){
+	$("#input").on("keypress",function( obj ){
 		if( obj.which===13)
 		{
 			// grabbing input val
-			var todotext=$(this).val();
+			var newToDo=$(this).val();
+			$(this).val("");
 			// adding a new li
-			$('ul').append("<li> <span> X </span>"+ todotext +" </li>");
+			if( newToDo!=="" )
+				$('ul').append('<li><span><i class="fa fa-trash-o"></i></span> ' + newToDo + '</li>'); // append
 		}
-	}),
+	});
 
-	);
+	$("#title").on("keypress",function(event) {
+		if( event.which===13)
+		{
+			// grabbing input val
+			var title=$(this).val();
+			// console.log(title);
+			$(this).val("");
+			// adding a new li
+			if( title!=="" )
+				$(this).attr('placeholder',title);  
+		}
+	});
+
+	$('.fa-pencil').click(function(){
+		$("#input").fadeToggle();
+	});
+
+});
